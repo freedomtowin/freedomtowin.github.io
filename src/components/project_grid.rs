222589453833ccfb78e5c01f7efe2e77b458f3f8
unknown <rohankotwani@gmail.com>
2025-07-05 17:02:07 -0400

    add rag search

--- src/components/project_grid.rs
+++ src/components/project_grid.rs
@@ -24,7 +24,7 @@ pub fn ProjectGrid() -> Element {
             div { class: "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6",
                 ProjectCard {
                     title: "Custom RAG FAQ Search, an AI Power Search Engine",
-                    description: "The purpose of this experiment was to see if I could build an AI powered search engine in 100% Rust and to see if a RAG powered search engine could be used to 1) run in WASM, or within the browser's memory and 2) produce good to decent results.",
+                    description: "An AI powered search engine in 100% Rust. A custom RAG powered search engine that 1) runs in WASM, or within the browser's memory and 2) produces good to decent results.",
                     image: FAQ_SEARCH,
                     tech_stack: vec!["Rust", "PyTorch", "Dioxus", "AWS"],
                     link: "https://medium.com/lazy-by-design/dioxus-faq-search-an-ai-power-search-engine-c7173d92d626",
